b4192a290e51ce1238c12e616e2b1778e74fbc1d
leewookeun <guny1213@gmail.com>
2020-05-30 12:01:44 +0900

    commit

--- js/demos.js
+++ js/demos.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
-    typeSpeed: 50,
+    typeSpeed: 80,
     backSpeed: 20,
     startDelay: 1000,
     loop: true,
